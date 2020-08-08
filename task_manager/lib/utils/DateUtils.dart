3e2044b2e0bc234928e76ad5cb3f3d84892bbee3
onurcan.timur <onurtalantimur@gmail.com>
2020-08-08 17:28:40 +0300

    Açıklayici yorum satirlari eklendi.

--- task_manager/lib/utils/DateUtils.dart
+++ task_manager/lib/utils/DateUtils.dart
@@ -1,7 +1,9 @@
 import 'package:intl/intl.dart';
 
+/*Yerel ozgun zaman formatlari icin gerekli araclar*/
 class DateUtils{
 
+  //girilen sayi 10'dan kucuk ise basina 0 getirir ve string olarak geri donderir
   String toDoubleDigit(int x){
 
     String str = x.toString();
@@ -12,19 +14,22 @@ class DateUtils{
     return str;
   }
 
+  //Parametre olarak verilen zamani ozgun formatta gostmermek icin string'e donusturur
   String dateFormatter(DateTime dateTime) {
 
     //tarih ve saati uygun formata ceviren method
     if (dateTime == null) return "";
 
-    var hour = dateTime.hour;
-    var minute = dateTime.minute;
+    var hour = dateTime.hour; //Saat
+    var minute = dateTime.minute; //dakika
 
-    var strHour, strMinute;
+    var strHour, strMinute; //Saat ve dakikayi S
 
     var result = DateFormat.MMMd('tr_TR')
         .format(dateTime); // ay ve gun turkiyeye uygun hale getliriliyor
 
+
+    //Donusumler yapiliyor
     strHour = toDoubleDigit(hour);
     strMinute = toDoubleDigit(minute);
 
@@ -35,10 +40,12 @@ class DateUtils{
 
 
 
+  //Parametre olarak verilen zamandan local gunu verir
+  //Ornek olarak 8/8/2020 => 'Cumartesi'
   String getLocalDay(DateTime dateTime){
 
-    return DateFormat.EEEE("tr_TR").format(dateTime);
-
+    return DateFormat.EEEE("tr_TR").format(dateTime); //Yerel gun formatina donusturuluyor
+                                                      //Ve geri donderiliyor
   }
 
 
