1f0c3d136356e02e9895107aa029743a56c52ea7
onurcan.timur <onurtalantimur@gmail.com>
2020-08-16 21:22:18 +0300

    SqlDate zamna formatlayıcısı eklendi.

--- lib/utils/DateUtils.dart
+++ lib/utils/DateUtils.dart
@@ -1,22 +1,18 @@
 import 'package:intl/intl.dart';
 
 /*Yerel ozgun zaman formatlari icin gerekli araclar*/
-class DateUtils{
-
+class DateUtils {
   //girilen sayi 10'dan kucuk ise basina 0 getirir ve string olarak geri donderir
-  String toDoubleDigit(int x){
-
+  String toDoubleDigit(int x) {
     String str = x.toString();
 
-    if(x<10)
-      str = "0"+str;
+    if (x < 10) str = "0" + str;
 
     return str;
   }
 
   //Parametre olarak verilen zamani ozgun formatta gostmermek icin string'e donusturur
   String dateFormatter(DateTime dateTime) {
-
     //tarih ve saati uygun formata ceviren method
     if (dateTime == null) return "";
 
@@ -28,26 +24,36 @@ class DateUtils{
     var result = DateFormat.MMMd('tr_TR')
         .format(dateTime); // ay ve gun turkiyeye uygun hale getliriliyor
 
-
     //Donusumler yapiliyor
     strHour = toDoubleDigit(hour);
     strMinute = toDoubleDigit(minute);
-
 
     return result +
         "   $strHour:$strMinute"; //saat tarihin yanına ilistiriliyor
   }
 
-
-
   //Parametre olarak verilen zamandan local gunu verir
   //Ornek olarak 8/8/2020 => 'Cumartesi'
-  String getLocalDay(DateTime dateTime){
-
-    return DateFormat.EEEE("tr_TR").format(dateTime); //Yerel gun formatina donusturuluyor
-                                                      //Ve geri donderiliyor
+  String getLocalDay(DateTime dateTime) {
+    return DateFormat.EEEE("tr_TR")
+        .format(dateTime); //Yerel gun formatina donusturuluyor
+    //Ve geri donderiliyor
   }
 
 
 
+  /*
+  Girilen dateTime verisini sql tarihinde donusturur.
+  Bu donusumde sadece tarih bilgilisi geri donderilir.
+
+  Ornek: String hali '1969-07-20 20:18:04Z' olan bir tarihten
+
+  '1969-07-20' geri donderilir.
+
+   */
+  String getSqlDateFormat(DateTime dateTime) { 
+    String strDateTime = dateTime.toString(); //Tarih string' donusturuluyor
+
+    return strDateTime.substring(0, 10); //ilk 10 karakter geri donderiliyor 
+  }                                       
 }
