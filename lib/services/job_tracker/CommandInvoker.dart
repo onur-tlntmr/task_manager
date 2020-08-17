e62047c0d0b505fe5a2469dee1adeb36f23ab8f3
onurcan.timur <onurtalantimur@gmail.com>
2020-08-18 00:44:32 +0300

    Yeni commandlar invoker'a eklendi.

--- lib/services/job_tracker/CommandInvoker.dart
+++ lib/services/job_tracker/CommandInvoker.dart
@@ -1,6 +1,8 @@
+import 'package:task_manager/services/job_tracker/AlarmCreateCommand.dart';
 import 'package:task_manager/services/job_tracker/CommandType.dart';
 import 'package:task_manager/services/job_tracker/ICommand.dart';
-import 'package:task_manager/services/job_tracker/StatusCommand.dart';
+import 'package:task_manager/services/job_tracker/StatusIncompleteCommand.dart';
+import 'package:task_manager/services/job_tracker/StatusRunningCommand.dart';
 
 /*
  * Bu sinif command'lara erisimi saglar.
@@ -14,7 +16,10 @@ class CommandInvoker {
   CommandInvoker() {
     commands = Map(); 
 
-    commands.putIfAbsent(CommandType.status, () => StatusCommand()); //Status command ekleniyor
+    commands.putIfAbsent(CommandType.statusIncomplete, () => StatusIncompleteCommand()); //Status command ekleniyor
+    commands.putIfAbsent(CommandType.statusRunning, () => StatusRunningCommand()); //Status command ekleniyor
+    commands.putIfAbsent(CommandType.alarmCreate, () => AlarmCreateComand()); //Status command ekleniyor
+
   }
 
   ICommand invoke(CommandType commandType) { //istenilen commandType'ine gore 
