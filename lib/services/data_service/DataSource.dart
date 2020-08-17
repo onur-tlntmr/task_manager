849d1cdc67d480689c35fd5cedc0e95a373d8b7b
onurcan.timur <onurtalantimur@gmail.com>
2020-08-17 12:50:50 +0300

    Ozel bir tarihteki tasklari getirme methodu eklendi.

--- lib/services/data_service/DataSource.dart
+++ lib/services/data_service/DataSource.dart
@@ -3,51 +3,59 @@ import 'package:task_manager/models/Task.dart';
 import 'package:task_manager/services/data_service/Observable.dart';
 import 'package:task_manager/services/data_service/Observer.dart';
 
-
 /*
 * Bu sinif DbHelperdaki crud operationlarin delege edildigi siniftir,
 * binaenaleyh kendisi db uzerindeki verileri guncellerken gerekli bagli oldugu
 * ui'lari da gunceller
 * Observable abstract sinifindan turemistir.
 * */
-class DataSource extends Observable{
-
+class DataSource extends Observable {
   static List<Observer> list;
   static DataSource _dataSource = DataSource._interial();
-  final DbHelper _dbHelper=DbHelper();
+  final DbHelper _dbHelper = DbHelper();
 
-  DataSource._interial(){ // Parametresiz constructor
+  DataSource._interial() {
+    // Parametresiz constructor
     list = List();
   }
 
-  factory DataSource(){ //singleton dataSource objesi donderir
+  factory DataSource() {
+    //singleton dataSource objesi donderir
     return _dataSource;
   }
 
   @override
-  void notifyObservers() { //Tum observer'larin  guncellemesini tetikleyen method
-    list.forEach((element) {element.update();});
+  void notifyObservers() {
+    //Tum observer'larin  guncellemesini tetikleyen method
+    list.forEach((element) {
+      element.update();
+    });
   }
 
   //Observer ekliyip cikaran methodlar
 
   @override
   void register(Observer o) {
-
     list.add(o);
-
   }
 
   @override
   void unregister(Observer o) {
-
     list.remove(o);
-
   }
 ////////////////////////////////////////
 
-  Future<List> getTasks() async{ //Tasklari donduren method
+///Tum tasklari geri donderir
+  Future<List> getTasks() async {
+    //Tasklari donduren method
     var list = await _dbHelper.getTasks();
+    return list;
+  }
+
+//Baslangic tarihi parametredeki degerle eslesen tarihtekileri geri donderir.
+// Daha fazlasi icin bakiniz: [DataSource.getTasksWithdate()]
+  Future<List> getTasksWithDate(DateTime dateTime) async {
+    var list = await _dbHelper.getTasksWithDate(dateTime);
     return list;
   }
 
@@ -57,23 +65,21 @@ class DataSource extends Observable{
   * sonucun 1 olmasi bir hata oldugunun gostergesidir
   * */
 
-  Future<int> insert(Task task) async{
+  Future<int> insert(Task task) async {
     var result = await _dbHelper.insert(task);
     notifyObservers();
     return result;
   }
 
-  Future<int> update(Task task) async{
+  Future<int> update(Task task) async {
     var result = await _dbHelper.update(task);
     notifyObservers();
     return result;
   }
 
-  Future<int> delete(Task task) async{
+  Future<int> delete(Task task) async {
     var result = await _dbHelper.delete(task.id);
     notifyObservers();
     return result;
   }
-
-
 }
